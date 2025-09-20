e0163f8c136a15502ca5cb48c565859ccdadd042
Aleksandra500 <aleksandracomagic7@gmail.com>
2025-09-20 15:45:31 +0200

    first file

---
+++ app/components/ProductCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddToCard from './AddToCard'
+
+function ProductCard() {
+  return (
+    <div>
+        <AddToCard/>
+    </div>
+  )
+}
+
+export default ProductCard
